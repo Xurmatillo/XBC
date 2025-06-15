7e61fa4acf3905f86576c8654761bf4de1be2729
Xurmatillo Oxunov ( Xurmatronix ) <96421607+Xurmatillo@users.noreply.github.com>
2025-06-15 23:34:42 +0500

    XurmatilloBluetoothCar2.0.ino

--- XurmatilloBluetoothCar.ino
+++ XurmatilloBluetoothCar.ino
@@ -1,114 +1,87 @@
-#include <AFMotor.h>
+#include <AFMotor.h> // XURMATILLO OXUNOV XIKMATULLAYEVICH 15/06/2025 23:34:27
+#include <SoftwareSerial.h>
 
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR34_1KHZ);
-AF_DCMotor motor4(4, MOTOR34_1KHZ);
+// Motor obyektlari
+AF_DCMotor motor1(1); // M1
+AF_DCMotor motor2(2); // M2
+AF_DCMotor motor3(3); // M3
+AF_DCMotor motor4(4); // M4
+
+// Bluetooth RX - pin 2, TX - pin 3
+SoftwareSerial BT(2, 3); // RX, TX BLUETOOTH'NI 2 VA 3 RAQAMLI ARDUINO PINIGA ULANG.
 
 char command;
 
-void setup()
-{
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+
+  // Motor tezligini maksimalga sozlash
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
 }
 
-void loop()
-{
-    if (Serial.available() > 0)
-    {
-        command = Serial.read();
-        Serial.println(command);
-        Stop();
-        
-        switch (command)
-        {
-            case 'F':  
-                forward();
-                break;
-            case 'B':  
-                back();
-                break;
-            case 'L':  
-                left();
-                break;
-            case 'R':
-                right();
-                break;
-        }
+void loop() {
+  if (BT.available()) {
+    command = BT.read();
+    Serial.println(command); // Debug uchun
+
+    switch (command) {
+      case 'F': // Oldinga
+        forward();
+        break;
+      case 'B': // Orqaga
+        backward();
+        break;
+      case 'L': // Chapga burilish (tank-style)
+        turnLeft();
+        break;
+      case 'R': // O‘ngga burilish (tank-style)
+        turnRight();
+        break;
+      case 'S': // To‘xtash
+        stopMotors();
+        break;
+      default:
+        stopMotors();
+        break;
     }
+  }
 }
 
-void forward()
-{
-    motor1.setSpeed(255);
-    motor1.run(FORWARD);
-    
-    motor2.setSpeed(255);
-    motor2.run(FORWARD);
-    
-    motor3.setSpeed(255);
-    motor3.run(FORWARD);
-    
-    motor4.setSpeed(255);
-    motor4.run(FORWARD);
+void forward() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
 }
 
-void back()
-{
-    motor1.setSpeed(255);
-    motor1.run(BACKWARD);
-    
-    motor2.setSpeed(255);
-    motor2.run(BACKWARD);
-    
-    motor3.setSpeed(255);
-    motor3.run(BACKWARD);
-    
-    motor4.setSpeed(255);
-    motor4.run(BACKWARD);
+void backward() {
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
 }
 
-void left()
-{
-    motor1.setSpeed(255);
-    motor1.run(BACKWARD);
-    
-    motor2.setSpeed(255);
-    motor2.run(BACKWARD);
-    
-    motor3.setSpeed(255);
-    motor3.run(FORWARD);
-    
-    motor4.setSpeed(255);
-    motor4.run(FORWARD);
+void turnLeft() {
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
 }
 
-void right()
-{
-    motor1.setSpeed(255);
-    motor1.run(FORWARD);
-    
-    motor2.setSpeed(255);
-    motor2.run(FORWARD);
-    
-    motor3.setSpeed(255);
-    motor3.run(BACKWARD);
-    
-    motor4.setSpeed(255);
-    motor4.run(BACKWARD);
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
 }
 
-void Stop()
-{
-    motor1.setSpeed(0);
-    motor1.run(RELEASE);
-    
-    motor2.setSpeed(0);
-    motor2.run(RELEASE);
-    
-    motor3.setSpeed(0);
-    motor3.run(RELEASE);
-    
-    motor4.setSpeed(0);
-    motor4.run(RELEASE);
+void stopMotors() {
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
 }
